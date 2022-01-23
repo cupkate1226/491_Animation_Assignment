ae137b63346bcdb0af9d406737afc6edfedf2a21
Katlyn <cup_kate@hotmail.com>
2022-01-23 14:36:42 -0800

    Adding Sprite sheet and starter code

---
+++ cat.js
@@ -0,0 +1,13 @@
+class Cat {
+    constructor(){
+
+    };
+
+    PaymentRequestUpdateEvent() {
+
+    };
+
+    draw(ctx) {
+        ctx.drawImage(ASSET_MANAGER.getAsset("./Cat Sprite Sheet.png"), 0, 0);
+    }
+}
