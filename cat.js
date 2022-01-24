d88c2026be9cd0e0bef1e48b01c9e6a1b380f23d
Katlyn <cup_kate@hotmail.com>
2022-01-23 22:38:21 -0800

    Fixing errors, adding background

--- cat.js
+++ cat.js
@@ -1,13 +1,20 @@
 class Cat {
-    constructor(){
-
+    constructor(game){
+        this.game = game;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./Cat_Sprite_Sheet.png"), 0, 128, 32, 32, 8, .1);
+        
+        this.x = -32;
+        this.y = 505;
+        this.speed = 100;
     };
 
-    PaymentRequestUpdateEvent() {
-
+    update() {
+        this.x += this.speed * this.game.clockTick;
+        if(this.x > 1024) this.x = -45;
     };
 
     draw(ctx) {
-        ctx.drawImage(ASSET_MANAGER.getAsset("./Cat Sprite Sheet.png"), 0, 0);
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        //ctx.drawImage(ASSET_MANAGER.getAsset("./Cat_Sprite_Sheet.png"), 0, 0, 550, 650);
     }
 }
--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./Cat Sprite Sheet.png");
+ASSET_MANAGER.queueDownload("./Cat_Sprite_Sheet.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
 
-	gameEngine.addEntity(new Cat());
+	gameEngine.addEntity(new Cat(gameEngine));
 
 	gameEngine.init(ctx);
 
